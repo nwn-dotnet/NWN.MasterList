87043eef637eadc437fb1747a472485e9389a24b
urothis <natecaple@gmail.com>
2020-12-26 00:43:32 -0600

    json properties

--- src/Server.cs
+++ src/Server.cs
@@ -7,43 +7,75 @@ using Newtonsoft.Json;
 namespace NWN.MasterList {
   public class Server {
     public class Manifest {
-      public bool required { get; set; } 
-      public string? hash { get; set; }
+      [JsonProperty("required")]
+      public bool Required { get; set; }
+      [JsonProperty("hash")]
+      public string? Hash { get; set; }
     }
 
     public class Nwsync {
-      public List<Manifest>? manifests { get; set; }
-      public string? url { get; set; }
+      [JsonProperty("manifests")]
+      public List<Manifest>? Manifests { get; set; }
+      [JsonProperty("url")]
+      public string? URL { get; set; }
     }
 
     public struct Data {
-      public int FirstSeen { get; set; } 
-      public int LastAdvertisement { get; set; } 
-      public string SessionName { get; set; } 
-      public string ModuleName { get; set; } 
-      public string ModuleDescription { get; set; } 
-      public bool Passworded { get; set; } 
-      public int MinLevel { get; set; } 
-      public int MaxLevel { get; set; } 
-      public int CurrentPlayers { get; set; } 
-      public int MaxPlayers { get; set; } 
-      public string Build { get; set; } 
-      public int Rev { get; set; } 
-      public int Pvp { get; set; } 
-      public bool Servervault { get; set; } 
-      public bool Elc { get; set; } 
-      public bool Ilr { get; set; } 
-      public bool OneParty { get; set; } 
-      public bool PlayerPause { get; set; } 
-      public int Os { get; set; } 
-      public int Language { get; set; } 
-      public int GameType { get; set; } 
-      public int Latency { get; set; } 
-      public string Host { get; set; } 
-      public int Port { get; set; } 
-      public string KxPk { get; set; } 
-      public string SignPk { get; set; } 
-      public string Connecthint { get; set; } 
+      [JsonProperty("first_seen")]
+      public int FirstSeen { get; set; }
+      [JsonProperty("last_advertisement")]
+      public int LastAdvertisement { get; set; }
+      [JsonProperty("session_name")]
+      public string SessionName { get; set; }
+      [JsonProperty("module_name")]
+      public string ModuleName { get; set; }
+      [JsonProperty("module_description")]
+      public string ModuleDescription { get; set; }
+      [JsonProperty("passworded")]
+      public bool Passworded { get; set; }
+      [JsonProperty("min_level")]
+      public int MinLevel { get; set; }
+      [JsonProperty("max_level")]
+      public int MaxLevel { get; set; }
+      [JsonProperty("current_players")]
+      public int CurrentPlayers { get; set; }
+      [JsonProperty("max_players")]
+      public int MaxPlayers { get; set; }
+      [JsonProperty("build")]
+      public string Build { get; set; }
+      [JsonProperty("rev")]
+      public int Revision { get; set; }
+      [JsonProperty("pvp")]
+      public int PVP { get; set; }
+      [JsonProperty("servervault")]
+      public bool ServerVault { get; set; }
+      [JsonProperty("elc")]
+      public bool ELC { get; set; }
+      [JsonProperty("ilr")]
+      public bool ILR { get; set; }
+      [JsonProperty("one_party")]
+      public bool OneParty { get; set; }
+      [JsonProperty("player_pause")]
+      public bool PlayerPause { get; set; }
+      [JsonProperty("os")]
+      public int OS { get; set; }
+      [JsonProperty("language")]
+      public int Language { get; set; }
+      [JsonProperty("game_type")]
+      public int GameType { get; set; }
+      [JsonProperty("latency")]
+      public int Latency { get; set; }
+      [JsonProperty("host")]
+      public string IP { get; set; }
+      [JsonProperty("port")]
+      public int Port { get; set; }
+      [JsonProperty("kx_pk")]
+      public string KxPk { get; set; }
+      [JsonProperty("sign_pk")]
+      public string SignPk { get; set; }
+      [JsonProperty("connecthint")]
+      public string ConnectHint { get; set; }
+      [JsonProperty("nwsync")]
       public Nwsync NwSync { get; set; }
     }
 
