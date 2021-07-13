117aa90600a633bd5000aa765d0d457aacd2aa69
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-12 20:54:46 -0700

    Xunit (#10)
    * readme
    
    * fix xunit
    
    * TestGetAllFirstSeen

--- Test/src/Client.Test.cs
+++ Test/src/Client.Test.cs
@@ -1,11 +1,21 @@
 ﻿using Xunit;
 
-namespace NWN.MasterList.Test {
-  public class Client {
-    [Fact]
-    public void GetServersTest() {
-      var connection = new MasterList.Client();
-      Assert.NotEmpty(connection.Servers);
+namespace NWN.MasterList.Test
+{
+    public class Client
+    {
+        [Fact]
+        public void GetServersTest()
+        {
+            var connection = new MasterList.Client();
+            Assert.NotEmpty(connection.Servers);
+        }
+
+        [Fact]
+        public void TestGetAllFirstSeen()
+        {
+            var connection = new MasterList.Client();
+            Assert.NotEmpty(Helper.GetAllFirstSeen(connection.Servers));
+        }
     }
-  }
 }
