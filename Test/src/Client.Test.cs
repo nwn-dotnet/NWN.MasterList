492b762b8f9d3048f369318123caad4b2b9eff7b
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-13 19:34:55 -0700

    TestGetAllFirstSeen

--- Test/src/Client.Test.cs
+++ Test/src/Client.Test.cs
@@ -1,9 +1,17 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace NWN.MasterList.Test
 {
     public class Client
     {
+        private readonly ITestOutputHelper output;
+
+        public Client(ITestOutputHelper output) => this.output = output;
+
         [Fact]
         public async void GetServersTest()
         {
@@ -16,8 +24,9 @@ namespace NWN.MasterList.Test
         [Fact]
         public async void TestGetAllFirstSeen()
         {
-            var connection = new MasterList.Client();
-            var json = await connection.GetAllFirstSeen();
+            var json = await new MasterList.Client().GetAllFirstSeen();
+            var getServers = await new MasterList.Client().GetServers();
+            Assert.Equal(json.Average(x => x.FirstSeen), getServers.Average(x => x.FirstSeen));
             Assert.NotEmpty(json);
             Assert.NotNull(json);
         }
