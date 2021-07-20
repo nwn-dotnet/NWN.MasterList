b3741c3bae3e267677a0a6905162dd73d3ab15b1
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-19 20:34:57 -0700

    Task<IEnumerable<NwServer>>

--- API/src/Server.cs
+++ API/src/Server.cs
@@ -10,7 +10,7 @@ namespace NWN.MasterList
     {
         public static string jsonUrl = "https://api.nwn.beamdog.net/v1/";
 
-        public static async Task<List<NwServer>> GetServers(this Client client)
+        public static async Task<IEnumerable<NwServer>> GetServers(this Client client)
         {
             string response = await Client.HttpClient.GetStringAsync($"{jsonUrl}/servers");
             return JsonSerializer.Deserialize<List<NwServer>>(response);
@@ -21,7 +21,6 @@ namespace NWN.MasterList
             string response = await Client.HttpClient.GetStringAsync($"{jsonUrl}/servers{publicKey}");
             return JsonSerializer.Deserialize<NwServer>(response);
         }
-
         public static async Task<NwServer> GetServer(this Client client, string ip, int port)
         {
             string response = await Client.HttpClient.GetStringAsync($"{jsonUrl}/servers/{ip}/{port}");
