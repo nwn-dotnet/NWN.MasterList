deb1ad33a84402782fd2bf537e15d4b755fc3b33
urothis <natecaple@gmail.com>
2021-10-17 02:52:31 -0500

    Dev (#20)
    * readme
    
    * url
    
    * Client (#9)
    
    * readme
    
    * Client
    
    * trim
    
    * Xunit (#10)
    
    * readme
    
    * fix xunit
    
    * TestGetAllFirstSeen
    
    * Update publish.yml
    
    * cleanup
    
    * forgot some ?'s
    
    * cleanup
    
    * Dev (#14)
    
    * settings.json
    
    * client test refactor, json settings
    
    * TestGetAllFirstSeen
    
    * OrderBy
    
    * GetAllLastAdvertisement
    
    * GetAllBuild
    
    * refactor to sort by name if value is same
    
    * GetAllConnectHint
    
    * PrintJson
    
    * issue, url
    
    * GetAllCurrentPlayersByModuleName
    
    * GetTotalPlayerCount, TestGetTotalPlayerCount
    
    * format
    
    * missed one
    
    * collection
    
    * TestGetAllElcByType.
    
    * GetAllGameType, TestGetAllGameType
    
    * GetAllGameTypeByType, TestGetAllGameTypeByType
    
    * fixed GetAllElcByType
    
    * fixed test
    
    * GetAllBuildByType, TestGetAllBuildByType
    
    * GetAllIrlByType, TestGetAllIrlByType
    
    * GetIRLAll
    
    * GetIpsAll
    
    * GetAllServersByIp
    
    * GetAllKxPk, TestGetAllKxPk
    
    * GetAllServersByKxPk
    
    * GetAllLanguage, TestGetAllLanguage
    
    * GetAllLanguageByType, TestGetAllLanguageByType
    
    * GetAllLatency, TestGetAllLatency
    
    * GetLatencyGreaterThan, TestGetLatencyGreaterThan
    
    * GetLatencyLesserThan
    
    * GetLatencyRange, TestGetLatencyRange
    
    * Get MaxLevel
    
    * MaxLevel
    
    * MinLevel
    
    * GetAllModuleDescription TestGetAllModuleDescriptio
    
    * GetAllNwSync
    
    * GetOnePartyByType, TestGetOnePartyByType
    
    * GetAllOSByType, TestGetAllOSByType
    
    * GetPasswordedByType, TestGetPasswordedByType
    
    * TestGetPlayerPauseByType, GetAllPlayerPause
    
    * TestGetAllByPortNumber
    
    * GetAllPvp, GetAllPvPByType
    
    * GetAllRevision, GetAllRevisionByType
    
    * GetAllServerVault, GetAllServerVaultByType
    
    * SessionName
    
    * TestGetAllSignPkByType, GetAllSignPkByType
    
    * moved functions to new file, test
    
    * removed unused
    
    * GettotalMaxPlayers
    
    * GetAllOsByTypePlayerDescending
    
    * GetTotalMaxPlayers typo
    
    * organize
    
    * corrections
    
    * GetBuildCountByBuildType, other functions
    
    * GetElcCountByType
    
    * GetGameTypeCountByType
    
    * GetILRCountByType GetIPCountByType
    
    * GetKxPkCountByType,GetLanguageCountByType
    
    * GetMaxLevelCountByType GetMinLevelCountByType
    
    * GetOnePartyCountByType GetOsCountByType
    
    * GetPasswordedCountByType GetPortCountByType
    
    * GetPVPCountByType GetRevisionCountByType
    
    * GetServerVaultCountBy GetSessionNameCountByType
    
    * GetSignPkCountByType
    
    * IsAllSameBuild IsAllSameElc IsAllSameGameType
    
    * IsAllSameILR IsAllSameIp IsAllSameKxPk
    
    * IsAllSame
    
    * IsAny
    
    * IsAny
    
    * GetAverage
    
    * FromUnixTime
    
    * TestFromUnixTime
    
    * Parse
    
    * parse edits
    
    * trim
    
    * corrections
    
    * GetAllNwSyncUrls
    
    * GetAllNwSyncUrls
    
    * update issue
    
    * Task<IEnumerable<NwServer>>
    
    * refactor Helper
    
    * test fixed
    
    * GetUniqueVersion
    
    * GetUniqueVersionCount
    
    * workaround for Do not use Assert.NotNull() on value type 'int'.
    
    * .vscode ignore
    
    * Delete launch.json
    
    * Delete settings.json
    
    * Delete tasks.json
    
    Co-authored-by: urothis <natecaple@gmail.com>
    
    Co-authored-by: Scott Milliorn <scottmilliorn@gmail.com>

---
+++ API/src/PrintJson.cs
@@ -0,0 +1,22 @@
+using System.Text.Json;
+namespace NWN.MasterList
+{
+    public static class PrintJson
+    {
+        // TODO #15
+        private const string path = @"";
+
+        //https://stackoverflow.com/a/63560258/11986604
+        private static string PrettyJson(string unPrettyJson)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(unPrettyJson);
+
+            return JsonSerializer.Serialize(jsonElement, options);
+        }
+    }
+}
