fbf37babe614185a24803679e0c0b989e7b59208
urothis <natecaple@gmail.com>
2021-10-31 05:53:41 +0000

    initial setup for 6.0

--- Test/src/Client.Test.cs
+++ Test/src/Client.Test.cs
@@ -1,21 +1,40 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace NWN.MasterList.Test
 {
-    public class Client
-    {
-        [Fact]
-        public async void GetServersTest()
-        {
-            var connection = new MasterList.Client();
-            Assert.NotEmpty(await connection.GetServers());
-        }
+  public static class Client
+  {
+    private readonly ITestOutputHelper output;
 
-        [Fact]
-        public async void TestGetAllFirstSeen()
-        {
-            var connection = new MasterList.Client();
-            Assert.NotEmpty(await connection.GetAllFirstSeen());
-        }
+    public Client(ITestOutputHelper output) => this.output = output;
+
+    [Fact]
+    public async void GetServersTest()
+    {
+      var connection = new MasterList.Client();
+      Assert.NotEmpty(await connection.GetServers());
     }
+
+    public static IOrderedEnumerable<Data.NwServer> OrderBy(this IOrderedEnumerable<Data.NwServer> servers)
+    {
+      return servers;
+    }
+
+    [Fact]
+    public async void TestGetAllFirstSeen()
+    {
+      var connection = new MasterList.Client();
+      var json = await connection.GetAllFirstSeen();
+      var test = await connection.GetServers().OrderByDescending(x => x.FirstSeen);
+      Assert.NotEmpty(json);
+      Assert.NotNull(json);
+
+      foreach (var item in json.OrderBy(x => x.FirstSeen))
+      {
+        output.WriteLine($"Module Name -> {item.ModuleName}\nFirst Seen -> {item.FirstSeen}\n");
+      }
+    }
+  }
 }
